e1db7c29213514fc3b5158aeca5d3a05af61bb3f
kitagawa <kitagawa@propagandist.co.jp>
2020-12-29 19:04:46 +0900

    マッパー、サービス追加

---
+++ demo/src/main/java/com/example/demo/mapper/ItemMapper.java
@@ -0,0 +1,46 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.domain.Item;
+
+/**
+ * アイテムマッパーインターフェース.
+ * @author kyokokitagawa
+ */
+@Mapper
+public interface ItemMapper {
+
+	/**
+	 * 全検索.
+	 * @return アイテムリスト
+	 */
+	List<Item> findAll();
+	
+	/**
+	 * 一件検索.
+	 * @param id ID
+	 * @return アイテム
+	 */
+	Item findone(Long id);
+	
+	/**
+	 * 保存.
+	 * @param item アイテム
+	 */
+	void save(Item item);
+	
+	/**
+	 * 更新.
+	 * @param item アイテム
+	 */
+	void update(Item item);
+	
+	/**
+	 * 削除.
+	 * @param id ID
+	 */
+	void delete(Long id);
+}
---
+++ demo/src/main/java/com/example/demo/service/ItemService.java
@@ -0,0 +1,70 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.domain.Item;
+import com.example.demo.mapper.ItemMapper;
+
+/**
+ * アイテムサービス.
+ * @author kyokokitagawa
+ */
+@Service
+public class ItemService {
+
+	/**
+	 * アイテムマッパー
+	 */
+	@Autowired
+	private ItemMapper itemMapper;
+	
+	/**
+	 * 全検索.
+	 * @return アイテムリスト
+	 */
+	@Transactional
+	public List<Item> findAll() {
+		return itemMapper.findAll();
+	}
+	
+	/**
+	 * 一件検索.
+	 * @param id ID
+	 * @return アイテム
+	 */
+	@Transactional
+	public Item findOne(Long id) {
+		return itemMapper.findone(id);
+	}
+	
+	/**
+	 * 保存.
+	 * @param item アイテム
+	 */
+	@Transactional
+	public void save(Item item) {
+		itemMapper.save(item);
+	}
+	
+	/**
+	 * 更新.
+	 * @param item アイテム
+	 */
+	@Transactional
+	public void update(Item item) {
+		itemMapper.update(item);
+	}
+	
+	/**
+	 * 削除.
+	 * @param id ID
+	 */
+	@Transactional
+	public void delete(Long id) {
+		itemMapper.delete(id);
+	}
+}
