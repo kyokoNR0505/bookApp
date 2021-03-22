4a47f15c6bb7a0b210e8322b0a113075b5a7e109
kyokoNR0505 <kyoko_kitagawa@outlook.jp>
2021-03-22 17:41:35 +0900

    テストコード追加

--- demo/src/test/java/com/example/demo/TaxCalculatorTests.java
+++ demo/src/test/java/com/example/demo/TaxCalculatorTests.java
@@ -16,7 +16,7 @@ public class TaxCalculatorTests {
 		BigDecimal price = BigDecimal.valueOf(1000);
 		BigDecimal expected = BigDecimal.valueOf(1100);
 		
-		// when
+		// whenS
 		BigDecimal actual = TaxCalculator.calcTaxIncluded(price);
 		
 		// then
@@ -24,7 +24,7 @@ public class TaxCalculatorTests {
 	}
 	
 	@Test
-	public void 税抜き金額が999円でを1098円返す() {
+	public void brew税抜き金額が999円でを1098円返す() {
 		// given
 		BigDecimal price = BigDecimal.valueOf(999);
 		BigDecimal expected = BigDecimal.valueOf(1098);
